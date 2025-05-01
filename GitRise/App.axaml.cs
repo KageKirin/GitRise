88f009fec437c669fe2d56d8e3daf021a7ae5bd9
KageKirin <kagekirin@gmail.com>
2025-05-01 23:14:09 +0900

    feat(GitRise): add IHost instance to App

--- GitRise/App.axaml.cs
+++ GitRise/App.axaml.cs
@@ -1,11 +1,14 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.Hosting;
 
 namespace GitRise;
 
 public partial class App : Application
 {
+    public IHost? GlobalHost { get; private set; }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
