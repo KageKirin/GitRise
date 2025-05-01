11fc065ed18b8ccd24177698a9ee95674e6a6658
Christian Helmich <kagekirin@gmail.com>
2025-05-02 02:54:36 +0900

    feat(GitRise): add Generic Host functionality (feat/hosting-prototype) [#5]
    - **build: add package reference to Microsoft.Extensions.Hosting**
     
    
    - **build: add package reference to Microsoft.Extensions.Hosting.Abstractions**
     
    
    - **build: add package reference to Microsoft.Extensions.Configuration**
     
    
    - **build: add package reference to Microsoft.Extensions.Configuration.Abstractions**
     
    
    - **build: add package reference to Microsoft.Extensions.DependencyInjection**
     
    
    - **build: add package reference to Microsoft.Extensions.Logging**
     
    
    - **build: add package reference to Microsoft.Extensions.Logging.Debug**
     
    
    - **build: add package reference to Alexinea.Extensions.Configuration.Toml**
     
    
    - **build: add package reference to KageKirin.Extensions.Configuration.GitConfig**
     
    
    - **feat(GitRise): add IHost instance to App**
     
    
    - **feat(GitRise): create IHost instance from preconfigured HostBuilder**
     
    
    - **feat(GitRise): set up Host to read configuration from `gitrise.toml`**
     
    
    - **feat(GitRise): set up Host to read configuration from git config**
     through GitConfigurationProvider
     
    
    - **feat(GitRise): set up Host to read configuration from GITRISE_ environment variables**
     
    
    - **feat(GitRise): set up Host to read configuration from command line arguments**
     
    
    - **feat(GitRise): set up Host to apply logging settings from configuration**
     
    
    - **feat(GitRise): set up Host to log to console**
     
    
    - **feat(GitRise): set up Host to log to debug stream**
     
    
    - **feat(GitRise): set Host minimum log level to trace**
     
    
    - **feat(GitRise): add MainWindow as transient service**
     
    
    - **feat(GitRise): dispose of IHost instance when application terminates**
     
    
    - **feat(GitRise): start IHost instance when application starts**

--- GitRise/App.axaml.cs
+++ GitRise/App.axaml.cs
@@ -1,23 +1,79 @@
+using System;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using KageKirin.Extensions.Configuration.GitConfig;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace GitRise;
 
 public partial class App : Application
 {
+    public IHost? GlobalHost { get; private set; }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
+        var hostBuilder = CreateHostBuilder();
+        GlobalHost = hostBuilder.Build();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = GlobalHost.Services.GetRequiredService<MainWindow>();
+            desktop.Exit += (sender, args) =>
+            {
+                GlobalHost.StopAsync(TimeSpan.FromSeconds(5)).GetAwaiter().GetResult();
+                GlobalHost.Dispose();
+                GlobalHost = null;
+            };
         }
 
         base.OnFrameworkInitializationCompleted();
+
+        await GlobalHost.StartAsync();
+    }
+
+    private static HostApplicationBuilder CreateHostBuilder()
+    {
+        //< below: create HostApplicationBuilder instance
+        // alternative: use Host.CreateDefaultBuilder
+        var hostBuilder = Host.CreateApplicationBuilder(Environment.GetCommandLineArgs());
+
+        //< below: configure where the Host configuration comes from
+        hostBuilder.Configuration.Sources.Clear();
+        hostBuilder.Configuration.SetBasePath(Environment.CurrentDirectory);
+        hostBuilder.Configuration.AddTomlFile(
+            Path.Join(AppDomain.CurrentDomain.BaseDirectory, "gitrise.toml"),
+            optional: true,
+            reloadOnChange: true
+        );
+        hostBuilder.Configuration.AddTomlFile(
+            Path.Join(Environment.CurrentDirectory, "gitrise.toml"),
+            optional: true,
+            reloadOnChange: true
+        );
+        hostBuilder.Configuration.AddGitConfig(path: Environment.CurrentDirectory, optional: false, reloadOnChange: true);
+        hostBuilder.Configuration.AddEnvironmentVariables(prefix: "GITRISE_");
+        hostBuilder.Configuration.AddCommandLine(Environment.GetCommandLineArgs());
+
+        //< below: configure how the Host should handle logging
+        hostBuilder.Logging.AddConfiguration(hostBuilder.Configuration.GetSection("Logging"));
+        hostBuilder.Logging.AddConsole(); //< add console as logging target
+        hostBuilder.Logging.AddDebug(); //< add debug output as logging target
+        hostBuilder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace); //< set minimum level to trace in Debug
+
+        //< below: register services to be available in Host
+        hostBuilder.Services.AddTransient<MainWindow>();
+
+        //< finally: return
+        return hostBuilder;
     }
 }
